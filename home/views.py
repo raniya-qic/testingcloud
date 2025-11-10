bc410ac6cfa2353343034d2f73cd3bedc8997218
raniya-qic <raniya.valliyil@qicgroup.com.qa>
2025-11-10 19:31:53 +0300

    run build errors

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from . models import *
 # Create your views here.
 
 def home_page(request):
+    nms=sampledb.objects.all()
     return render(request,'home.html')
